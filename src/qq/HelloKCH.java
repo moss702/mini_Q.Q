de4b49d5f3799009660277640bab621d5313a6cb
shonjaeki <jaeki.shon@gmail.com>
2025-04-30 17:48:03 +0900

    같다

--- src/qq/HelloKCH.java
+++ src/qq/HelloKCH.java
@@ -3,5 +3,6 @@ package qq;
 public class HelloKCH {
 	public static void main(String[] args) {
 		System.out.println("인생");
+		System.out.println("같다");
 	}
 }
--- src/qq/HelloSJK.java
+++ src/qq/HelloSJK.java
@@ -3,6 +3,7 @@ package qq;
 public class HelloSJK {
 	public static void main(String[] args) {
 		System.out.println("115194");
+		
 	}
 
 }
