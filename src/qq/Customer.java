befaf07e203d628051725c482f9f9d4b7aec14b9
shonjaeki <jaeki.shon@gmail.com>
2025-05-04 10:01:30 +0900

    Main,customer 수정완료

--- src/qq/Customer.java
+++ src/qq/Customer.java
@@ -1,5 +1,41 @@
 package qq;
 
-public class Customer extends UserAcount {
 
-}
+public class Customer {
+	public Customer getLoginCustomer() {
+		return getLoginCustomer();
+	}
+	
+	public void register() {
+		System.out.println("등록가능");
+	
+	}
+	public void read() {
+		System.out.println("조회 기능");
+//		print();
+	}
+	public void remove() {
+		System.out.println("삭제 기능");
+	}
+	public void mypage() {
+		System.out.println("마이페이지 기능");
+	}
+	public void cupon() {
+		System.out.println("쿠폰발급 기능");
+	}
+	public void end() {
+		System.out.println("종료");
+	}
+	}
+
+	
+
+	
+		
+		
+		
+
+
+	
+	
+	
--- src/qq/Main.java
+++ src/qq/Main.java
@@ -3,15 +3,30 @@ import java.util.Scanner;
 
 public class Main  {
 	public static void main(String[] args) {
-		System.out.println("================== 학생 점수 관리 프로그램 ==================");
+		System.out.println("================== 손님 관리 프로그램 ==================");
 		for(;;) {
 			try {
-				switch (Util.nextInt("1. 로그인 2.회원가입" )) {
+				switch (Util.nextInt("1.예약 2.등록 3.조회 4.삭제 5.마이페이지 6.쿠폰발급 7. 종료" )) {
 				case 1:
 					
 					break;
 				case 2:
 		
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					
+					break;
+				case 5:
+					
+					break;
+				case 6:
+					
+					break;
+				case 7:
+					System.out.println("bye~!");
 					break;
 				
 				default:
