8e51c0f3bccadeede17d4b50649485b68bb2c742
shonjaeki <jaeki.shon@gmail.com>
2025-05-04 17:20:06 +0900

    Customer 수정

--- src/qq/Customer.java
+++ src/qq/Customer.java
@@ -1,7 +1,18 @@
 package qq;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
+	private List<UserService> userServices = new ArrayList<UserService>();
+//	private UserService() {}
+//	public UserService. () {
+//		return UserService;
+		public void mypage() {
+			System.out.println("내정보보기");
+//			UserService.getInstance().print(UserService.getInstance().findByID(null));
+		
+	}
 	
 	}
 
