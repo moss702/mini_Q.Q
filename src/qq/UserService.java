80041aa17f4eedaee954c018c6317cf304eb9037
moss702 <seodam27@gmail.com>
2025-05-05 14:22:52 +0900

    UserService.findByCustomer 메소드 작성 (Customer 선수정 및 main 동작 테스트필요)

--- src/qq/UserService.java
+++ src/qq/UserService.java
@@ -1,8 +1,8 @@
 package qq;
 
-//userServiece : 회원가입, 로그인
-//c
-//seller 회원정보조회, 사업자 여부 변경
+//userServiece : 회원가입, 로그인, 로그아웃, 내정보수정
+//Customer 내정보조회
+//Seller 회원정보조회, 사업자 여부 변경
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,22 +13,28 @@ public class UserService {
 	private List<UserAccount> accounts = new ArrayList<>();
 	private List<UserAccount> sortedAccounts; // 소비금액순, 회원번호순 정렬 변경을 위한 리스트입니다.
 	
+	public List<UserAccount> getAccounts() { //일단 다른 클래스에서 접근할 수 있게 만들어뒀는데
+	    return accounts;					 //Customer 접근할 메서드도 따로 만들었기 때문에, 불필요시 삭제!
+	}
+	
 	private static UserService userService = new UserService();
+	
 	private UserService() {}
 	public static UserService getInstance() {
 		return userService;
 	}
 
-	private UserAccount loginUser; //로그인 상태 저장          ***** 소비금액 가져오려면 UserAccount가 아니라 Customer 타입으로 가져와야 하나?
+	private UserAccount loginUser; //로그인 상태 저장
 	public UserAccount getLoginUser() {
 		return loginUser;
 	}
 	
 	
 	
-	{ //임시 데이터 ***** 필요기능!! 회원가입한 Accounts의 데이터파일 생성 및 저장
+	{ //임시 데이터 ***** 추가필요!! 회원가입한 Accounts의 데이터파일 생성 및 저장
 		accounts.add(new UserAccount(1, "더미사장", "admin", "1234", true));
-		accounts.add(new UserAccount(2, "더미손님", "dummy", "1234", false));
+		accounts.add(new UserAccount(2, "더미고객", "dummy", "1234", false));
+		accounts.add(new UserAccount(3, "고양이", "cat", "1234", false));
 		
 		sortedAccounts = new ArrayList<>(accounts);
 	}
@@ -64,13 +70,29 @@ public class UserService {
 	public UserAccount findByID(String id) {
 	UserAccount account = null;
 	for (int i = 0; i < accounts.size(); i++) {
-		if (accounts.get(i).getId() == id) {
+		if (accounts.get(i).getId().equals(id)) {
 			account = accounts.get(i);
 			break;
 		}
 	}	
 	return account;
 	}
+	
+	//-----------------서치_ID (Customer) : (타 클래스에서 Customer 클래스의 누적소비금액 필드 접근을 위한 메서드)
+//	public Customer findByCustomer(String id) {
+//	    for (UserAccount ua : accounts) {
+//	        if (ua instanceof Customer && ua.getId().equals(id)) {
+//	            return (Customer) ua;
+//	        }
+//	    }
+//	    return null;
+//	}
+
+//	타 클래스 호출 예
+//	Customer c = UserService.getInstance().findByCusto(id); //이름들이 길어서 좀 길긴한데.. 이름을 더 짧게 수정해야 할까요?
+//	int total = c.getTotalSpend(); //Customer 클래스의 누적소비금액 필드 접근.
+// 	Customer 클래스에서 private 누적소비금액 필드 및 게터/세터 만들어주세요! * 소비금액 변수명 협의 및 확정 필요
+		
 	//-----------------출력_회원목록
 	public void print(List<UserAccount> UA) {
 		UA.forEach(System.out::println);
@@ -120,6 +142,12 @@ public class UserService {
 		}
 	}
 	
+	//-----------------로그아웃(Customer, Seller)
+	public void logout() {
+	    loginUser = null;
+	    System.out.println("[로그아웃 되었습니다]");
+	}
+	
 	//-----------------내 정보 수정하기(Customer, Seller)
 	public void modify() { //수정가능요소 : ID, PW, name
 		System.out.println("=======[내 회원정보 수정]=======");
@@ -152,12 +180,11 @@ public class UserService {
 		System.out.println("(임시) 내 정보 보기 (임시)");
 	}
 	
-	
 //===============================Seller 전용=====================
 	
 	//-----------------회원목록 조회(only Seller)
 	///학생예제에서는 조회,석차순조회 별도 메뉴탭이었지만...
-	/// [1. 뒤로가기] [2. 번호순(소비순)] 번갈아가며 보여주면 UX면에서 좋을것같아요! * 플머님 구현가능한가요?
+	/// [1. 뒤로가기] [2. 번호순(소비순)] 번갈아가며 보여주면 UX면에서 좋을것같아요! * 플머님 구현가능한가요? 일단 함 해보죠
 
 	public void read() { //회원번호순 조회
 		System.out.println("=======[회원 목록 조회]=======");
@@ -199,9 +226,8 @@ public class UserService {
 		} else {
 			System.out.println("[(!) 존재하지 않는 ID입니다.]");
 		}
-		/// ********** if문 뎁스가 조금 깊죠..? ^^;;;;;; 관련한 코드 수정 피드백 언제나 환영합니다.
+		// ********** if문 뎁스가 조금 깊죠..? ^^;;;;;; 관련한 코드 수정 피드백 언제나 환영합니다.
 	}
-
 	
 //====================================================UserService class end line
 }
