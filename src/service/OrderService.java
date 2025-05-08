0e8c9db37401932659cf1c4c5e973471ad5d9742
BreakTime94 <kimchan9408@gmail.com>
2025-05-08 19:50:56 +0900

    Order, OrderService, Cart 수정

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -6,14 +6,17 @@ import Domain.Cart;
 import Domain.Customer;
 import Domain.Menu;
 import Domain.Order;
+import qq.Util;
 public class OrderService {
+	
+	
 	private static final OrderService ORDER_SERVICE = new OrderService();
+	private CustomerService cu = CustomerService.getInstance();
 	private OrderService() {	}
 	public static OrderService getInstance() {
 		return ORDER_SERVICE;
 	}
-	
-	List<Order> orders = new ArrayList<Order>();
+	List<Order> orders = new ArrayList<Order>(); // 영수증 정보
 	{
 		
 	}
@@ -21,20 +24,32 @@ public class OrderService {
 	
 	// 주문하기 (장바구니 담기+주문하기)
 	public void makeOrder() {
-		List<Cart> carts = new ArrayList<>();
-		MenuService.getInstance().read();
-		// 상품 번호를 입력받고
-		Menu m = MenuService.getInstance().findBy(0);
-		// 수량 입력
-		int amount = 2;
-		Cart cart = new Cart(m, amount);
-		carts.add(cart);
-		
-		Order order = new Order(null, carts);
-		orders.add(order);
-		
-		// 결제 프로세스 진행
-		order.pay = true;
+		while(true) {
+			List<Cart> carts = new ArrayList<>();
+//			MenuService.getInstance().read();
+			// 상품 번호를 입력받고
+			int no = Util.nextInt("주문하실 메뉴 번호를 입력하세요 > ");
+			Menu m = MenuService.getInstance().findBy(no);
+			// 수량 입력
+			int amount = Util.nextInt("담을 수량을 입력하세요 > ");
+			Cart cart = new Cart(m, amount);
+			carts.add(cart);
+			if(Util.nextConfirm("메뉴판으로 돌아가시겠습니까?(y/n) > ")) {
+				break;
+			}
+			int sales = 0;
+			for(Cart c : carts) {
+				sales += c.getAmount() * c.getPrice();
+			}
+//			결제 프로세스 진행
+			System.out.println("결제하실 금액은 " + sales + "원 입니다."); 
+			Util.nextInt(sales + "원을 입력하여주세요. > ");
+			System.out.println("결제가 완료되었습니다.");
+			Order order = new Order(null, carts, sales); // null에는 login된 customer를 가져와야 함
+			orders.add(order);
+			order.pay = true;
+			return;
+		}
 	}
 	
 	
