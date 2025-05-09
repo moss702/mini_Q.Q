f9a97f009ce4951624fb81d1d0511f22072716bd
moss702 <seodam27@gmail.com>
2025-05-09 18:12:32 +0900

    Merge branch 'master' of https://github.com/songseongjun/Q.Q.git

---
+++ src/utils/QqUtils.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.util.Scanner;
+
+public class QqUtils {
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	public static String nextLine(String msg) {
+		System.out.print(msg);
+		return scanner.nextLine();
+	}
+	
+	public static int nextInt(String msg) {
+		return Integer.parseInt(nextLine(msg));
+	}
+	
+	public static boolean nextConfirm(String msg) {
+		String s = nextLine(msg);
+		return s.equalsIgnoreCase("y") || s.equalsIgnoreCase("yes");
+	}
+}
