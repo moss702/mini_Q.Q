e5fdeeba9511737a529e9b54bef948bd3c771748
shonjaeki <so125697@gmail.com>
2025-05-09 17:12:47 +0900

    commit 수정 하는중

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -7,7 +7,13 @@ public class Customer extends User {
 	private String id;
 	private String pw;
 	
+		// 문자열을 객체 생성방법에 따른 차이
+		// getter 외부로 전달하는 메서드
+		// setter 외부에서 메서드에 접근하여 조건에 맞을 경우 데이터 값을 변경할 수 있는 메서드
+		// to string 객체가 가지고 있는 정보나 값들을 문자열로 만들어 리턴하는 메소드
+
 	public Customer() {}
+		
 	
 	public Customer (int no, String name, String email, String id, String pw) {
 		
@@ -54,8 +60,6 @@ public class Customer extends User {
 		this.pw = pw;
 	}
 	
-
-
 
 @Override
 public String toString() {
