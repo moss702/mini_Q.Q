d2c9703e004dc6ad88a7f84050fafefad2e07e1b
BreakTime94 <kimchan9408@gmail.com>
2025-05-09 18:24:35 +0900

    패키지 수정사항

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -2,13 +2,12 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import domain.Cart;
 import domain.Customer;
 import domain.Menu;
 import domain.Order;
 import domain.User;
-import qq.Util;
+import utils.QqUtils;
 
 public class OrderService {
 	
@@ -30,13 +29,13 @@ public class OrderService {
 			List<Cart> carts = new ArrayList<>();
 			MenuService.getInstance().read();
 			// 상품 번호를 입력받고
-			int no = Util.nextInt("주문하실 메뉴 번호를 입력하세요 > ");
-			Menu m = MenuService.getInstance().findBy(no + 1); // 숫자 번호는 1번부터
+			int no = QqUtils.nextInt("주문하실 메뉴 번호를 입력하세요 > ");
+			Menu m = MenuService.getInstance().findBy(no); // 숫자 번호는 1번부터
 			// 수량 입력
-			int amount = Util.nextInt("담을 수량을 입력하세요 > ");
+			int amount = QqUtils.nextInt("담을 수량을 입력하세요 > ");
 			Cart cart = new Cart(m, amount);
 			carts.add(cart);
-			if(Util.nextConfirm("메뉴판으로 돌아가시겠습니까?(y/n) > ")) {
+			if(QqUtils.nextConfirm("메뉴판으로 돌아가시겠습니까?(y/n) > ")) {
 				break;
 			}
 			int sales = 0;
@@ -45,7 +44,7 @@ public class OrderService {
 			}
 //			결제 프로세스 진행
 			System.out.println("결제하실 금액은 " + sales + "원 입니다."); 
-			Util.nextInt(sales + "원을 입력하여주세요. > ");
+			QqUtils.nextInt(sales + "원을 입력하여주세요. > ");
 			System.out.println("결제가 완료되었습니다.");
 //			private Customer lc = cu.loginCustomer;
 			Order order = new Order(null, carts, sales); // null에는 login된 customer 즉 lc를 가져와야 함
@@ -56,9 +55,9 @@ public class OrderService {
 	}
 	// 결제 취소
 	public void cancle() {
-		int no = Util.nextInt("결제를 취소하실 메뉴 번호를 선택하여 주십시오. > ");
+		int no = QqUtils.nextInt("결제를 취소하실 메뉴 번호를 선택하여 주십시오. > ");
 		
-		int amount = Util.nextInt("결제를 취소하실 수량을 선택하여 주십시오. > ");
+		int amount = QqUtils.nextInt("결제를 취소하실 수량을 선택하여 주십시오. > ");
 		
 	}
 	
@@ -74,9 +73,9 @@ public class OrderService {
 	
 	public static void main(String[] args) {// 구동 연습 메서드
 		OrderService order = ORDER_SERVICE.getInstance();
-		
-		int no = Util.nextInt(" 1. 주문하기, 2. 결제 취소하기 3. findBy점검 ");
-		switch (no) {
+		while(true) {
+			int no = QqUtils.nextInt(" 1. 주문하기, 2. 결제 취소하기 3. findBy점검 4. 종료");
+			switch (no) {
 			case 1 :  order.makeOrder();
 			break;
 			
@@ -85,6 +84,7 @@ public class OrderService {
 			
 			case 3 : order.findByPayment(new Customer());
 			break;
+			}
 		}
 	}
 }
