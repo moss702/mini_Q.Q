b2df322d93d7d83351dbaf19867a496ffe67292f
BreakTime94 <kimchan9408@gmail.com>
2025-05-12 20:46:48 +0900

    병합

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -9,6 +9,7 @@ import domain.User;
 import service.UserService;
 
 import java.lang.annotation.Target;
+import java.text.ParseException;
 import java.util.List;
 
 import domain.Admin;
@@ -28,17 +29,17 @@ public class AdminService {
 		return ADMIN_SERVICE;
 	}
 	
-	public void adminInit() {
+	public void adminInit() throws Exception {
 
 		System.out.println("===============관리자 로그인 상태");
-		int input = nextInt("[1.회원목록 조회] [2.관리자 등급 관리] [3.회원삭제] [4.메뉴관리] [0.로그아웃]");	
+		int input = nextInt("[1.회원목록 조회] [2.관리자 등급 관리] [3.회원삭제] [4.메뉴관리] [5.매출조회] [0.로그아웃]");	
 		switch (input) {
 		
 			case 1 : 
 				UserService.getInstance().printUser();
 				break;
 			case 2 : 
-				System.out.println("* 임시 * 관리자등급관리");
+				isSeller();
 				break;
 			case 3 : 
 				System.out.println("* 임시 * 회원삭제");
@@ -46,24 +47,26 @@ public class AdminService {
 			case 4 : 
 				System.out.println("* 임시 * 메뉴관리"); 
 				break;
+			case 5 : 
+				System.out.println("* 임시 * 매출조회"); 
+				OrderService.getInstance().findBySalesDate();
+				break;
 			case 0 :
 				UserService.getInstance().logout();
 				break;
 		}
 	}
 
-
-
 	//-----------------관리자 권한 부여
 	//User customer에 있던 정보를 User Admin으로 이동시키기
 	public void isSeller() {
 		String id = nextLine("[관리자 권한을 부여할 ID를 입력하세요] > ");
 		User t = UserService.getInstance().findBy(id, User.class);
-		if (t == UserService.getInstance().getUsers(Admin.class)){
+		if (UserService.getInstance().getUsers(Admin.class).contains(t)){
 			System.out.println("[(!)이미 관리자 등급인 계정입니다.]");
-		} else if (t == UserService.getInstance().getUsers(Customer.class)) {
+		} else if (UserService.getInstance().getUsers(Customer.class).contains(t)) {
 			UserService.getInstance().users.add(new Admin(t.getUserNo(),t.getId(),t.getPw(),t.getName()));
-			//UserService.getInstance().users.remove(Customer(t.getUserNo(),t.getId(),t.getPw(),t.getName()));
+		//	UserService.getInstance().users.remove(Customer t(t.getUserNo(),t.getId(),t.getPw(),t.getName()));
 			System.out.printf("[id : \"%s\"에 관리자 권한이 부여되었습니다.]", id);
 		} else {
 			System.out.println("[(!)존재하지 않는 계정입니다.]");
