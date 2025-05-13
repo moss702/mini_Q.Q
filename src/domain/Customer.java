caa9d580205cfe6dde4879534b66ed42f84beffd
shonjaeki <jaeki.shon@gmail.com>
2025-05-13 14:56:11 +0900

    customer 오버라이딩 삭제

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -5,14 +5,6 @@ public class Customer extends User {
 	public Customer(int userNo, String id, String pw, String name) {
 		super(userNo, id, pw, name);
 	}
-
-	@Override
-	public String toString() {
-		return "Customer [getUserNo()=" + getUserNo() + ", getName()=" + getName() + ", getId()=" + getId()
-				+ ", getPw()=" + getPw() + ", toString()=" + super.toString() + ", getClass()=" + getClass()
-				+ ", hashCode()=" + hashCode() + "]";
-	}
-
 	public void setPassword(String newPw) {
 		// TODO Auto-generated method stub
 		
